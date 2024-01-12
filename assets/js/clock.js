da410ce770e5c82f97b2258aba3e8a1611ca51a4
ayazayazov <ayazayazov00@gmail.com>
2024-01-13 02:22:53 +0400

    upload

---
+++ assets/js/clock.js
@@ -0,0 +1,334 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     // API'den verileri çek
+//     fetch("https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec")
+//         .then(response => response.json())
+//         .then(data => {
+//             // API'den gelen verileri kullanarak saat bilgilerini oluştur
+//             const worldClockContainer = document.getElementById("worldClockContainer");
+
+//             data.forEach(entry => {
+//                 const paralelContainer = document.createElement("div");
+//                 paralelContainer.classList.add("paralel-container");
+
+//                 const h2 = document.createElement("h2");
+//                 h2.classList.add("h22", "text-white");
+//                 h2.textContent = entry.location;
+
+//                 const h3 = document.createElement("h3");
+//                 h3.classList.add("h33", "text-white");
+//                 h3.textContent = entry.time;
+
+//                 paralelContainer.appendChild(h2);
+//                 paralelContainer.appendChild(h3);
+//                 worldClockContainer.appendChild(paralelContainer);
+//             });
+//         })
+//         .catch(error => console.error("Error fetching world time:", error));
+// });
+
+
+let containerid = document.querySelector("#containerid");
+
+// async function containerapi (){
+//     try{let url = `http://worldtimeapi.org/api/timezone/Asia/`;
+//     const response = await fetch(url);
+
+//     const data = await response.json();
+
+//     let apidataall = data.PromiseResult.map((element)=>{
+//         console.log(element, "element");
+//     }).join('');}catch{
+
+//     }
+  
+// };
+
+// console.log(containerapi());
+
+const worldClocks = document.querySelector('#worldClocks');
+
+const BakuPromise = fetch('http://worldtimeapi.org/api/timezone/Asia/Baku');
+
+BakuPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML = `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Baku</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const IstanbulPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Istanbul');
+
+IstanbulPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Istanbul</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const BerlinPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Berlin');
+
+BerlinPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Berlin</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const DublinPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Dublin');
+
+DublinPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Dublin</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const NYCPromise = fetch('http://worldtimeapi.org/api/timezone/America/New_York');
+
+NYCPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">New York</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const LosAngelesPromise = fetch('http://worldtimeapi.org/api/timezone/America/Los_Angeles');
+
+LosAngelesPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Los Angeles</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const BudapestPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Budapest');
+
+BudapestPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Budapest</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const LondonPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/London');
+
+LondonPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">London</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const MoscowPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Moscow');
+
+MoscowPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Moscow</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+
+const RomePromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Rome');
+
+RomePromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Rome</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+const MadridPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Madrid');
+
+MadridPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Madrid</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
+
+// const MadridPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Madrid');
+
+// MadridPromise.then((res)=>{
+//     const secondPromise = res.json();
+//     return secondPromise;
+// }).then((data)=>{
+//     console.log(data);
+//     // console.log('datetime', data.datetime.slice(11, 16));
+//     worldClocks.innerHTML += `
+//     <div class="line"></div>
+//     <div class="paralel-container">
+//       <h2 class="h22 text-white">Madrid</h2>
+//       <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+//     </div>
+//     `
+
+// }).catch((err)=>{
+//     console.log('err', err);
+// }).finally(()=>{
+//     console.log('fetched.');
+// })
+
+const ViennaPromise = fetch('http://worldtimeapi.org/api/timezone/Europe/Vienna');
+
+ViennaPromise.then((res)=>{
+    const secondPromise = res.json();
+    return secondPromise;
+}).then((data)=>{
+    console.log(data);
+    // console.log('datetime', data.datetime.slice(11, 16));
+    worldClocks.innerHTML += `
+    <div class="line"></div>
+    <div class="paralel-container">
+      <h2 class="h22 text-white">Vienna</h2>
+      <h3 class="h33 text-white">${data.datetime.slice(11, 16)}</h3>
+    </div>
+    `
+
+}).catch((err)=>{
+    console.log('err', err);
+}).finally(()=>{
+    console.log('fetched.');
+})
---
+++ assets/js/stopwatch.js
@@ -0,0 +1,87 @@
+// Buttonlar
+let lap = document.querySelector(".lap");
+let lapBtn = document.querySelector("#lapBtn");
+let start = document.querySelector(".start");
+let stop = document.querySelector(".stop");
+let reset = document.querySelector(".reset");
+const lapList = document.querySelector("#lapList");
+// Esas Timer
+let timer = document.querySelector(".timer");
+
+let seconds = 0;
+let milliseconds = 0;
+let intervalIndex;
+let count = 0;
+
+let lapdiv = document.querySelector(".lapdiv");
+
+lapBtn.disabled = true
+
+start.addEventListener("click", () => {
+  start.style = "display: none;";
+  stop.style = "display: inline;";
+  lap.style = "display: inline;";
+  reset.style = "display: none;";
+
+  lapBtn.disabled = false
+
+  clearInterval(intervalIndex);
+  intervalIndex = setInterval(() => {
+    milliseconds += 10;
+
+    if (milliseconds === 1000) {
+      seconds++;
+      milliseconds = 0;
+    }
+
+    timer.innerHTML = convertTime(seconds, milliseconds);
+  }, 10);
+});
+
+stop.addEventListener("click", () => {
+  stop.style = "display: none;";
+  start.style = "display: inline;";
+  lap.style = "display: none;";
+  reset.style = "display: inline;";
+
+  clearInterval(intervalIndex);
+
+  timer.innerHTML = convertTime(seconds, milliseconds);
+});
+
+reset.addEventListener("click", () => {
+  reset.style = "display: none;";
+  lap.style = "display: inline;";
+
+  lapBtn.disabled = true
+
+  clearInterval(intervalIndex);
+  seconds = 0;
+  milliseconds = 0;
+
+  lapList.innerHTML = ""
+  count = 0
+
+  timer.innerHTML = convertTime(seconds, milliseconds);
+});
+
+function convertTime(seconds, milliseconds) {
+  const hours = Math.floor(seconds / 3600);
+  const remainingSeconds = seconds % 3600;
+  const min = Math.floor(remainingSeconds / 60);
+  const lastSec = remainingSeconds - min * 60;
+
+  const roundHours = hours;
+  const roundMin = min < 10 ? "0" + min : min;
+  const roundLastSec = lastSec < 10 ? "0" + lastSec : lastSec;
+
+  const roundMilliseconds = milliseconds.toString().padStart(3, "0");
+
+  let formattedTime = `${roundHours}:${roundMin}:${roundLastSec},${roundMilliseconds}`;
+
+  return formattedTime;
+}
+lapBtn.addEventListener("click", () => {
+  count++
+  lapList.innerHTML += `<div class="Lap"><p>Lap ${count}</p> ${convertTime(seconds,milliseconds)}</div>`;
+});
